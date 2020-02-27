a41de52f2660d817d8b81176bf5d37b8c15f6879
Ricardo Reais <Ricardo.Reais@vortal.biz>
2020-02-27 18:50:31 +0000

    Added basic login page with redirect to chat module

---
+++ src/app/shared/models/user.model.ts
@@ -0,0 +1,5 @@
+export interface User {
+    name: string;
+    messagesCount: number;
+    language: string;
+}
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { User } from 'src/app/shared/models/user.model';
+import { UserService } from 'src/app/shared/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public userForm: FormGroup;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+    this.userForm = new FormGroup({
+      // TODO: Add some more user name validations.
+      name: new FormControl('', Validators.required)
+    });
   }
 
+  register(user: User) {
+    console.log(user);
+
+    this.userService.register(user);
+    this.router.navigate(['']);
+  }
 }
